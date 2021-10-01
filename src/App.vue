f66b7d6721129e4478ffe17f96e57bd05e431dbd
wangning <wangning.wn0822@bytedance.com>
2021-10-01 17:01:21 +0800

    make app body height 100%

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <n-layout>
+  <n-layout embedded style="height: 100%;">
     <n-layout-header bordered>
       <nav-bar>
       </nav-bar>
     </n-layout-header>
-    <n-layout-content embedded content-style="padding: 24px;">
+    <n-layout-content content-style="padding: 24px;">
         <router-view/>
     </n-layout-content>
-    <n-layout-footer embedded>成府路</n-layout-footer>
   </n-layout>
 </template>
 <script>
@@ -19,16 +18,29 @@ export default {
 }
 </script>
 <style>
+html,body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 
 #nav {
   padding: 30px;
+}
+
+.n-layout-content {
+  background-color: transparent;
 }
 
 #nav a {
