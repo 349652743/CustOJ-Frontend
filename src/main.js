3048549fce11c3b06f94cc4a8c21ea83fe529e5d
wangning <wangning.wn0822@bytedance.com>
2021-10-07 18:27:18 +0800

    reset ajust plugin

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from '@/App.vue'
 import router from '@/router'
 import naive from 'naive-ui'
 import 'vfonts/Lato.css'
-import 'lib-flexible'
 // import {
 // // create naive ui
 // create,
